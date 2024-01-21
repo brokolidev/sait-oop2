386779518d49ca7f3c469188f3a671ad0dde549c
brokolidev <brokoli@marketboro.com>
2024-01-21 14:53:05 -0700

    create Person class

---
+++ lab1/lab1/Person.cs
@@ -0,0 +1,18 @@
+namespace lab1;
+
+public class Person
+{
+    private int personId;
+    private string firstName;
+    private string lastName;
+    private string favoriteColour;
+    private int age;
+    private bool isWorking;
+
+    protected string DisplayPersonInfo(
+        int personId, string firstName, string lastName, 
+        string favoriteColour, int age, bool isWorking)
+    {
+        return $"";
+    }
+}
