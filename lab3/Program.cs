66abbe4f2c28d23f168dd55a92a5a89ebbb69b14
Hyunseung Choi <bocalist@gmail.com>
2024-01-30 09:54:30 -0700

    implemented part1

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please enter the name of the new dog.");
+        string dogName = Console.ReadLine();
+
+        Dog newDog = new Dog();
+        newDog.SetName(dogName);
+        newDog.SetColour("Brown");
+        newDog.SetAge(5);
+
+        Console.WriteLine($"The new dog! Name: {newDog.Name} / Age: {newDog.Age} / Colour: {newDog.Colour}");
+        newDog.Eat();
+        
+        Console.WriteLine("Please enter the name of the new cat.");
+        string catName = Console.ReadLine();
+        
+        Cat newCat = new Cat();
+        newCat.SetName(catName);
+        newCat.SetColour("Red");
+        newCat.SetAge(7);
+
+        Console.WriteLine($"The new cat! Name: {newCat.Name} / Age: {newCat.Age} / Colour: {newCat.Colour}");
+        newCat.Eat();
     }
 }
