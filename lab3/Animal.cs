5a5d51cf3ef66c267d0820a471c861e53d66c9a1
Hyunseung Choi <bocalist@gmail.com>
2024-01-30 09:41:10 -0700

    created abstract Animal class

---
+++ lab3/Animal.cs
@@ -0,0 +1,25 @@
+﻿namespace lab3;
+
+public abstract class Animal
+{
+    public string Name { get; set; }
+    public string Colour { get; set; }
+    public int Age { get; set; }
+
+    public void SetName(string name)
+    {
+        this.Name = name;
+    }
+
+    public void SetColour(string colour)
+    {
+        this.Colour = colour;
+    }
+
+    public void SetAge(int age)
+    {
+        this.Age = age;
+    }
+
+    public abstract void Eat();
+}
